4bfca11371ec6dd2dcdb4bc0cc51f54f3c928c95
Pavel Barysevich <pavelbarysevich@gmail.com>
2020-03-03 12:31:56 +0300

    1. The sum of two numbers

---
+++ Swiftme-FirstApp/func.swift
@@ -0,0 +1,12 @@
+//
+//  func.swift
+//  Swiftme-FirstApp
+//
+//  Created by Павел Борисевич on 2/27/20.
+//  Copyright © 2020 Павел Борисевич. All rights reserved.
+//
+
+import Foundation
+func sum(_ a: String?, _ b: String?) -> Int {
+    return Int(a!)! + Int(b!)!
+}
